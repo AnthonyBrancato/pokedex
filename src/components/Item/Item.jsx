2d2ca5508d59761e50c8c83ae022198844c07510
AnthonyBrancato <abrancato.contact@gmail.com>
2021-07-03 15:33:43 +0200

    add: box shadow to Item / MontSerrat font

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -4,19 +4,21 @@ const Item = ({ item }) => {
   let altAttribute = `${item.name} is displayed in a card`;
 
   return (
-    <div className="item-card">
-      <div className="item-image">
-        <img
-          src={
-            item.sprites.versions["generation-v"]["black-white"].animated
-              .front_default
-          }
-          alt={altAttribute}
-        />
-      </div>
-      <div className="item-info">
-        <p className="item-name">{item.name}</p>
-        <p className="item-id">N.{item.id}</p>
+    <div className="item-wrapper">
+      <div className="item-card">
+        <div className="item-image">
+          <img
+            src={
+              item.sprites.versions["generation-v"]["black-white"].animated
+                .front_default
+            }
+            alt={altAttribute}
+          />
+        </div>
+        <div className="item-info">
+          <p className="item-name">{item.name}</p>
+          <p className="item-id">N.{item.id}</p>
+        </div>
       </div>
     </div>
   );
